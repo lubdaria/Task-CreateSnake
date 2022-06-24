153393852112f9f6dc5717e2f791d8b21eed729e
lubdaria <98103429+lubdaria@users.noreply.github.com>
2022-06-24 16:59:34 +0300

    addNewFunction

--- task.js
+++ task.js
@@ -3,59 +3,38 @@
 // 4 3 2 
 // Result: 5 3 4 3 1 6 8 2 2  
 
-let arrayNumbers = [[1, 4, 7],
-                    [2, 5, 8],
-                    [3, 6, 9]];
+let numberArray = [[1, 8, 9, 16],
+                   [2, 7, 10, 15],
+                   [3, 6, 11, 14],
+                   [4, 5, 12, 13]];
                     
-let arrays = createArraysWhithArrays(arrayNumbers);
-let numberSnake = createStrickWithSnakeArrays(arrays);
-let listNumbers = showNumbers(numberSnake);
-console.log(listNumbers);
+let snakeArray = createSnakeArray(numberArray);
+let numbers = showStrickWithSnakeArrays(snakeArray);
+console.log(numbers);
 
-function createArraysWhithArrays(numbers){
-  const numbersOne = [];
-  const numbersSecond = [];
-  const numbersThirth = [];
-  let tempNumber = '';
-  
-  for(let i = 0; i < numbers.length; i++){
-    let iteam = numbers[i];
-
-    for(let i = 0; i < iteam.length; i++){
-        
-      if(i === 0){
-        numbersOne.push(iteam[i]);
-      }
-      else if(i === 1){
-        numbersSecond.push(iteam[i]);
-      }
-      else if(i === 2){
-        numbersThirth.push(iteam[i]);
+function createSnakeArray(arr){
+  const numberArray = [];
+      
+  for(let i = 0; i < arr[0].length; i++){
+            
+    if(i % 2 === 0){
+      for(let j = 0; j < arr.length; j++){
+        numberArray.push(arr[j][i]);
       }
     }
-  }
-
-  return [numbersOne, numbersSecond, numbersThirth];
-}
-  
-function createStrickWithSnakeArrays(num){
-  let tempNumbers = '';
-  
-  for(let i = 0; i < num.length; i++){
-    tempNumbers = tempNumbers + num[i];
-  }
-
-  return tempNumbers;
-}
-
-function showNumbers(num){
-  let numbers = '';
-
-  for(let i = 0; i < num.length; i++){
-    if(Number(num[i])){
-      numbers = numbers + num[i] + ' ';
+    else{
+      for(let j = arr.length - 1; j >= 0; j--){
+        numberArray.push(arr[j][i]);
+      }
     }
-  }
+  }  
 
+  return numberArray;
+}
+ 
+function showStrickWithSnakeArrays(arr){
+  let numbers = arr.join(' ');
+    
   return numbers;
 }
+
